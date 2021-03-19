e95a70b05f5969ddc77867783d1c4d8b99086be7
Daniel Nunes <danielzcomputacao@gmail.com>
2021-03-19 11:16:29 -0300

    aplicando css, e primeiro component

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import './index.css'
+import React from 'react'
+import ReactDOM from 'react-dom'
+import Primeiro from './components/basicos/Primeiro'
+    
+
+    ReactDOM.render(
+        <div>
+            <Primeiro></Primeiro>
+        </div>,
+        document.getElementById('root')
+        )
