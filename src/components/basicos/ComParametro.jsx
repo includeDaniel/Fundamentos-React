6aa8e2be7bc828e41b847b2732639c36214bb886
Daniel Nunes <danielzcomputacao@gmail.com>
2021-03-19 12:28:58 -0300

    passando parametros em componentes

---
+++ src/components/basicos/ComParametro.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export default function ComParametro(props) {
+    console.log(props)
+        const titulo = props.titulo
+        const aluno = props.aluno
+        const nota = props.nota
+        const status = props.nota >= 7 ? 'Aprovado' : 'Reprovado'
+    return (
+        <div>
+            <h2> { titulo } </h2>
+            <p> { aluno } que tem nota { nota } foi { status } </p> 
+        </div>
+
+       
+    )    
+}
--- src/index.js
+++ src/index.js
@@ -2,11 +2,22 @@ import './index.css'
 import React from 'react'
 import ReactDOM from 'react-dom'
 import Primeiro from './components/basicos/Primeiro'
+import ComParametro from './components/basicos/ComParametro'
     
-
-    ReactDOM.render(
-        <div>
+ReactDOM.render(
+    
+        <div id="app">
             <Primeiro></Primeiro>
+            <ComParametro
+                titulo="Nota do Aluno"
+                aluno="Daniel"
+                nota={10}>
+            </ComParametro>
+            <ComParametro
+                titulo="Nota do Aluno"
+                aluno="Cristiano"
+                nota={6}>
+            </ComParametro>
         </div>,
         document.getElementById('root')
-        )
+    )
