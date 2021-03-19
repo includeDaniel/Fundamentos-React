bfa2bf466d37964ca692afda8dd3457e5cdcb649
Daniel Nunes <danielzcomputacao@gmail.com>
2021-03-19 11:41:14 -0300

    jsx em componentes

--- src/components/basicos/Primeiro.js
+++ src/components/basicos/Primeiro.js
@@ -1,3 +1,12 @@
+import React from 'react'
+
+const msg = 'Seja bem-vindo'
+
 export default function Primeiro() {
-    return 'Primeiro Componente'
+    return (
+       <div>
+            <h2>Primeiro Componente</h2>
+            <p>{ msg }</p>
+        </div>
+   )
 }
